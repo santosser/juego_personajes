6dd72c6dd70adbf88936889d15bec1bd6cf1101d
santosser <santos02.root@gmail.com>
2017-07-24 15:05:57 -0500

    creando la clases misil

---
+++ src/colisiones/misil.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colisiones;
+public class misil extends Sprite {
+
+    private final int BOARD_WIDTH = 390;
+    private final int MISSILE_SPEED = 2;
+
+    public misil(int x, int y) {
+        super(x, y);
+
+        initMissile();
+    }
+    
+    private void initMissile() {
+        
+        loadImage("misil.png");
+        getImageDimensions();        
+    }
+
+    public void move() {
+        
+        x += MISSILE_SPEED;
+        
+        if (x > BOARD_WIDTH)
+            vis = false;
+    }
+}
