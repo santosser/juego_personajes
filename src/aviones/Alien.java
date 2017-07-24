bcac197e9282cdec21940e584b8371451a77df9b
santosser <santos02.root@gmail.com>
2017-07-23 21:43:51 -0500

    madificando la clases Alias

--- src/aviones/Alien.java
+++ src/aviones/Alien.java
@@ -1,15 +1,25 @@
-package aviones;
+public class Alien extends Sprite {
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    private final int INITIAL_X = 400;
 
-/**
- *
- * @author LENOVO
- */
-public class Alien {
-    
+    public Alien(int x, int y) {
+        super(x, y);
+
+        initAlien();
+    }
+
+    private void initAlien() {
+
+        loadImage("alien.png");
+        getImageDimensions();
+    }
+
+    public void move() {
+
+        if (x < 0) {
+            x = INITIAL_X;
+        }
+
+        x -= 1;
+    }
 }
