0ddf18405c15ab4385dcfe80e95fe76294e5898f
santosser <santos02.root@gmail.com>
2017-07-23 21:11:28 -0500

    creando variables y las dimenciones de la imagen que vamos ha cargar

--- src/aviones/Sprite.java
+++ src/aviones/Sprite.java
@@ -1,7 +1,27 @@
-
-package aviones;
-
+import java.awt.Image;
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
 
 public class Sprite {
-    
+
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+    protected boolean vis;
+    protected Image image;
+
+    public Sprite(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+        vis = true;
+    }
+
+    protected void getImageDimensions() {
+
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+    }
+
 }
