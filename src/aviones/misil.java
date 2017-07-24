3ca92a4e52d17261a8575cfd10cc67d9e0770558
santosser <santos02.root@gmail.com>
2017-07-23 21:24:00 -0500

    mejorando la clase misil

--- src/aviones/misil.java
+++ src/aviones/misil.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package aviones;
+public class misil extends Sprite {
 
-/**
- *
- * @author LENOVO
- */
-public class misil {
+    private final int BOARD_WIDTH = 390;
+    private final int MISSILE_SPEED = 2;
+
+    public misil(int x, int y) {
+        super(x, y);
+
+        initMissile();
+    }
     
+    private void initMissile() {
+        
+        loadImage("misil.png");
+        getImageDimensions();        
+    }
+
+    public void move() {
+        
+        x += MISSILE_SPEED;
+        
+        if (x > BOARD_WIDTH)
+            vis = false;
+    }
 }
